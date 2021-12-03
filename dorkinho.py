14d6d12f64a73b1b9a4d2d6abe8e29d188663fae
SidHawks <65626547+sidhawkss@users.noreply.github.com>
2021-12-03 10:41:54 -0300

    First Commit

---
+++ dorkinho.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import json
+import webbrowser
+import sys
+
+chrome_path = 'C:\Program Files\Google\Chrome\Application\chrome.exe %s'
+
+def list_dorks():
+   with open("./dorks.json","r") as file:
+      counter = 0
+      file = json.load(file)
+      names = (list(file.keys()))
+      for name in names:
+         print(name) 
+
+def select_dork(name_dork, url):
+   print(f"Name: {name_dork} \nURL:  {url}")
+   with open("./dorks.json","r") as file:
+      counter = 0
+      file = json.load(file)
+      names = (list(file.keys()))
+      while counter < len(names):
+         if (name_dork == list(file.keys())[counter]): 
+            print(str(file[names[counter]]).format(url))
+         counter += 1
+
+def all_dorks_request(url):
+   with open("./dorks.json","r") as file:
+      counter = 0
+      file = json.load(file)
+      names = (list(file.keys()))
+      while counter < len(names):
+         print(str(file[names[counter]]).format(url))
+         req = str(file[names[counter]]).format(url)
+
+         webbrowser.open(req)
+         counter += 1
+
+if __name__ == "__main__":
+   try:
+      if str(sys.argv[1]) == "list_dorks":
+         list_dorks()
+      if str(sys.argv[1]) == "all_dorks_request":
+         all_dorks_request(str(sys.argv[2]))
+      if str(sys.argv[1]) == "select_dork":
+         select_dork(str(sys.argv[2]), str(sys.argv[3]))
+   except:
+      
+      print(r"""
+     ____      U  ___ u   ____      _  __                _   _     _   _     U  ___ u 
+    |  _"\      \/"_ \/U |  _"\ u  |"|/ /       ___     | \ |"|   |'| |'|     \/"_ \/ 
+   /| | | |     | | | | \| |_) |/  | ' /       |_"_|   <|  \| |> /| |_| |\    | | | | 
+   U| |_| |\.-,_| |_| |  |  _ <  U/| . \\u      | |    U| |\  |u U|  _  |u.-,_| |_| | 
+    |____/ u \_)-\___/   |_| \_\   |_|\_\     U/| |\u   |_| \_|   |_| |_|  \_)-\___/  
+     |||_         \\     //   \\_,-,>> \\,-.-,_|___|_,-.||   \\,-.//   \\       \\    
+    (__)_)       (__)   (__)  (__)\.)   (_/ \_)-' '-(_/ (_")  (_/(_") ("_)     (__)   
+      
+   [*] The usage of the program is: python dorkinho.py OPTION
+   
+   Options:
+            list_dorks                           #List all dorks name. 
+            all_dorks_request URL                #Open the browser with all dorks.
+            select_dork NAME URL_PARAMETER       #Create one dork.
+      """)
+
+
+
